de49ef3f65e34613d8341f8ac9ab93f11aa97b61
CoreShiftD <278102187+CoreShiftD@users.noreply.github.com>
2026-07-09 18:08:32 +0000

    build.sh: append baseband_guard to CONFIG_LSM via sed post-merge_config
    Signed-off-by: CoreShiftD <278102187+CoreShiftD@users.noreply.github.com>

--- scripts/apply-bbg.sh
+++ scripts/apply-bbg.sh
@@ -10,16 +10,3 @@ git clone --depth=1 ${REF:+--branch "$REF"} "$REPO" "$KERNEL_DIR/Baseband-guard"
 cd "$KERNEL_DIR"
 sh Baseband-guard/setup.sh
 echo "CONFIG_BBG=y" >> "$KERNEL_DIR/common/CoreShift.fragment"
-
-FRAGMENT="$KERNEL_DIR/common/CoreShift.fragment"
-if grep -q 'CONFIG_DEFAULT_SECURITY_SMACK=y' "$KERNEL_DIR/common/arch/arm64/configs/gki_defconfig"; then
-  echo 'CONFIG_LSM="lockdown,yama,loadpin,safesetid,integrity,smack,selinux,tomoyo,apparmor,bpf,baseband_guard"' >> "$FRAGMENT"
-elif grep -q 'CONFIG_DEFAULT_SECURITY_APPARMOR=y' "$KERNEL_DIR/common/arch/arm64/configs/gki_defconfig"; then
-  echo 'CONFIG_LSM="lockdown,yama,loadpin,safesetid,integrity,apparmor,selinux,smack,tomoyo,bpf,baseband_guard"' >> "$FRAGMENT"
-elif grep -q 'CONFIG_DEFAULT_SECURITY_TOMOYO=y' "$KERNEL_DIR/common/arch/arm64/configs/gki_defconfig"; then
-  echo 'CONFIG_LSM="lockdown,yama,loadpin,safesetid,integrity,tomoyo,bpf,baseband_guard"' >> "$FRAGMENT"
-elif grep -q 'CONFIG_DEFAULT_SECURITY_DAC=y' "$KERNEL_DIR/common/arch/arm64/configs/gki_defconfig"; then
-  echo 'CONFIG_LSM="lockdown,yama,loadpin,safesetid,integrity,bpf,baseband_guard"' >> "$FRAGMENT"
-else
-  echo 'CONFIG_LSM="lockdown,yama,loadpin,safesetid,integrity,selinux,smack,tomoyo,apparmor,bpf,baseband_guard"' >> "$FRAGMENT"
-fi
